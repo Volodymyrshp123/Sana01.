a1b91aafb36e0540f86f310dad577df145090ed9
Volodymyr Shponarsky <ipz241_shvp@student.ztu.edu.ua>
2024-11-01 20:57:00 +0200

    Sana01

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b, c, d,z,r,x,y;
+        Console.WriteLine("enter a: ");
+        string input = Console.ReadLine();
+        a = double.Parse(input);
+        Console.WriteLine("enter b: ");
+        string input1 = Console.ReadLine();
+        b = double.Parse(input1);
+        Console.WriteLine("enter c: ");
+        string input2 = Console.ReadLine();
+        c = double.Parse(input2);
+        Console.WriteLine("enter d: ");
+        string input3 = Console.ReadLine();
+        d = double.Parse(input3);
+       x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+        Console.WriteLine(x);
+        y = (5 * (a + b) * (c - d)) / (0.5 * c) + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+        Console.WriteLine(y);
+        z = (Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b);
+        Console.WriteLine(z);
+        r = (0.5 * a + 0.75 * b - 1.4) / (3 * c + 1) + 1 / (a - c);
+        Console.WriteLine(r);
+
+    }
+}
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double m, n,z1,z2;
+        Console.WriteLine("enter n: ");
+        string input = Console.ReadLine();
+        n = double.Parse(input);
+        Console.WriteLine("enter m: ");
+        string input1 = Console.ReadLine();
+        m = double.Parse(input1);
+        z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) /
+                     Math.Sqrt(Math.Pow(m, 3) * n + n * m + Math.Pow(m, 2) - m);
+        Console.WriteLine(z1);
+        z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+        Console.WriteLine(z2);
+        double  x, b, a;
+        Console.WriteLine("enter x: ");
+        string input2 = Console.ReadLine();
+        x = double.Parse(input2);
+        Console.WriteLine("enter b: ");
+        string input3 = Console.ReadLine();
+        b = double.Parse(input3);
+        Console.WriteLine("enter n: ");
+        string input4 = Console.ReadLine();
+        a = double.Parse(input4);
+        double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) +
+                      (a - b) * Math.Pow(Math.Sin(a - b), 2) +
+                      Math.Pow(10, -2) * (x - b);
+        Console.WriteLine(y);
+    }
+}
+       
